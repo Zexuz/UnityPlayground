3ab0e6d6fb8f17567791d2db4a1caeb0169b42b9
Robin Edbom <robin.edbom@gmail.com>
2017-09-07 15:46:36 +0200

    Fixed my misstakes

---
+++ Assets/Scripts/MessageSenderBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageSenderBehaviour : MonoBehaviour {
+	public Modifier Modifier;
+
+	public string data;
+	public float interval = 0.0f;
+
+	private float time = 0.0f;
+
+	// Update is called once per frame
+	void Update() {
+		time += Time.deltaTime;
+		if (interval > 0.0f)
+		{
+			if (time >= interval)
+			{
+				Modifier.PrintMessage(data);
+				time = 0.0f;
+			}
+		}
+	}
+}
