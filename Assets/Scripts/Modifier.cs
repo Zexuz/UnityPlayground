888557584b948c49938b38233699df20e2a8e7cf
Simon Holm <simon.holm@cybercom.com>
2017-09-07 17:13:13 +0200

    Cleaned up spawn structure.

--- Assets/Scripts/Modifier.cs
+++ Assets/Scripts/Modifier.cs
@@ -5,118 +5,20 @@ using UnityEngine;
 
 public class Modifier : MonoBehaviour
 {
-    public GameObject GameObjectPrefab;
-    public float ShrinkFactor = 1;
-    public float IncreseFactor = 1;
-
-    private Dictionary<string, GameObjectNode> _nodes;
-    public int LifeTime = 1;
+    public GameObject Node;
 
     private void Start()
     {
-        _nodes = new Dictionary<string, GameObjectNode>();
+        
     }
 
     public void PrintMessage(string messageString)
     {
-        if (!_nodes.ContainsKey(messageString))
-        {
-            var messageWeapper = new MsgWrapper(messageString);
-            var gObj = Instantiate(GameObjectPrefab, new Vector3(), Quaternion.identity);
-            _nodes[messageString] = new GameObjectNode(messageWeapper, gObj);
-        }
-        _nodes[messageString].MsgWrapper.AddToList(new Message(LifeTime));
-        _nodes[messageString].GameObject.transform.localScale *= IncreseFactor;
+        Node.SendMessage("NewMessage", messageString);        
     }
 
     private void Update()
     {
-        var keysToRemove = new List<string>();
-        foreach (var kvp in _nodes)
-        {
-            var gameObjectNode = kvp.Value;
-            gameObjectNode.MsgWrapper.Update();
-            var messageCount = gameObjectNode.MsgWrapper.Messages.Count;
-            if (messageCount == 0)
-                keysToRemove.Add(kvp.Key);
-
-            if (gameObjectNode.GameObject.transform.localScale.magnitude < 0.5f)
-                keysToRemove.Add(kvp.Key);
-
-            var factor = (messageCount * ShrinkFactor) - messageCount + 1;
-            Debug.Log(factor);
-            gameObjectNode.GameObject.transform.localScale /= factor;
-        }
-
-        foreach (var key in keysToRemove)
-        {
-            Destroy(_nodes[key].GameObject);
-            _nodes.Remove(key);
-        }
-    }
-}
-
-
-public class GameObjectNode
-{
-    public MsgWrapper MsgWrapper { get; private set; }
-    public GameObject GameObject { get; private set; }
-
-    public GameObjectNode(MsgWrapper msgWrapper, GameObject gameObject)
-    {
-        MsgWrapper = msgWrapper;
-        GameObject = gameObject;
-    }
-}
-
-
-public class MsgWrapper
-{
-    public string MessageString { get; private set; }
-    private List<Message> _messages;
-
-    public List<Message> Messages
-    {
-        get { return _messages; }
-    }
-
-    public MsgWrapper(string messageString)
-    {
-        MessageString = messageString;
-
-        _messages = new List<Message>();
-    }
-
-    public void AddToList(Message message)
-    {
-        _messages.Add(message);
-    }
-
-    public void Update()
-    {
-        for (int i = _messages.Count - 1; i >= 0; i--)
-        {
-            if (_messages[i].ShouldDestory())
-                _messages.RemoveAt(i);
-        }
-    }
-}
-
-public class Message
-{
-    public DateTime Created { get; private set; }
-    public int LifeTime { get; private set; }
-
-
-    public Message(int lifeTime)
-    {
-        Created = DateTime.Now;
-        LifeTime = lifeTime;
-    }
-
-    public bool ShouldDestory()
-    {
-        var timeLeft = Created.AddSeconds(LifeTime) - DateTime.Now;
-        return timeLeft.TotalMilliseconds < 0;
+    
     }
 }
---
+++ Assets/Scripts/NodeBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeBehaviour : MonoBehaviour {
+
+    public int id;
+    public GameObject MessageBlob;
+    public GameObject CenterOfGravity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void NewMessage(string data) {
+        var obj = Instantiate(MessageBlob);
+        obj.GetComponent<Attract>().attractedTo = CenterOfGravity;
+    }
+}
