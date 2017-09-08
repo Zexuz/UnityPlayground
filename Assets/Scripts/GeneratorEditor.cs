2c68ad368d16190d80fc996b933211d39e813082
Robin Edbom <robin.edbom@gmail.com>
2017-09-08 13:50:05 +0200

    Fixed stuff so we can deplay on hololens

--- Assets/Scripts/GeneratorEditor.cs
+++ Assets/Scripts/GeneratorEditor.cs
@@ -1,7 +1,8 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
-
+using System.Linq;
 [CustomEditor(typeof(Generator))]
 public class GeneratorEditor : Editor
 
@@ -9,6 +10,8 @@ public class GeneratorEditor : Editor
     string res = "";
     string data = "0000000000000000";
     float interval = 0.0f;
+
+    private int number = 1;
 
     List<GameObject> senders = new List<GameObject>();
 
@@ -28,11 +31,24 @@ public class GeneratorEditor : Editor
         if (GUILayout.Button("Create Sender"))
         {
             senders.Add(generator.createSender());
-
         }
 
+        number = EditorGUILayout.IntField(number);
+        if (GUILayout.Button("Create random messages sender"))
+        {
+            for (int i = 0; i < number; i++)
+            {
+                var messageSender = generator.createSender();
+                MessageSenderBehaviour script = messageSender.GetComponent<MessageSenderBehaviour>();
+                script.data = RandomString(1);
+                script.interval = Random.value;
+                senders.Add(messageSender);
+            }
+        }
+        
+
         List<GameObject> toRemove = new List<GameObject>();
-        foreach(GameObject sender in senders)
+        foreach (GameObject sender in senders)
         {
             MessageSenderBehaviour script = sender.GetComponent<MessageSenderBehaviour>();
             script.data = EditorGUILayout.TextField("Data", script.data);
@@ -44,9 +60,19 @@ public class GeneratorEditor : Editor
             }
         }
 
-        foreach (GameObject sender in toRemove) {
+        foreach (GameObject sender in toRemove)
+        {
             senders.Remove(sender);
             Destroy(sender);
         }
     }
+    
+    private static System.Random random = new System.Random();
+    public static string RandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
 }
+#endif
---
+++ Assets/StartScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour
+{
+
+
+	public Generator Generator;
+	
+	// Use this for initialization
+	void Start () {
+		
+		Debug.Log("start");
+		for (int i = 0; i < 100; i++)
+		{
+			var messageSender = Generator.createSender();
+			MessageSenderBehaviour script = messageSender.GetComponent<MessageSenderBehaviour>();
+			script.data = RandomString(1);
+			script.interval = Random.value;
+		}
+	}
+	
+	private static System.Random random = new System.Random();
+	public static string RandomString(int length)
+	{
+		const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		return new string(Enumerable.Repeat(chars, length)
+			.Select(s => s[random.Next(s.Length)]).ToArray());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
