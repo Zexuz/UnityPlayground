475e617984c4c2c77b680a60456434d3037e707f
Robin Edbom <robin.edbom@gmail.com>
2017-09-15 14:27:47 +0200

    Last minute fix for the demo.

---
+++ Assets/ToogleArCam.cs
@@ -0,0 +1,25 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class ToogleArCam : MonoBehaviour, IInputClickHandler
+{
+    public GameObject ARCamera;
+    public TextMesh Text;
+
+    // Use this for initialization
+    void Start()
+    {
+        UpdateText();
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        ARCamera.SetActive(!ARCamera.activeSelf);
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        Text.text = string.Format("AR STATUS: {0}", ARCamera.activeSelf ? "on" : "off");
+    }
+}
